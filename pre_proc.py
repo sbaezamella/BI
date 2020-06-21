6a593446fd8ca0911acc53bcbc5da2ab7edfa334
Sebastián Baeza <sbaezamella@gmail.com>
2020-06-21 17:42:01 -0400

    Cambios en train y prec

--- pre_proc.py
+++ pre_proc.py
@@ -1,48 +1,46 @@
-"""
-"""
 import pandas as pd
 import numpy as np
 
 
-data = pd.read_csv('data.csv', sep=',', header=None)
-data_input= data.iloc[0:1600, 0:256]
+def pre_proc():
+    data = pd.read_csv('data.csv', sep=',', header=None)
+    data_input = data.iloc[0:1600, 0:256]
 
-print(data_input)
+    data_label = np.zeros((1600, 10))
+    data_label_prev = data.iloc[0:1600, 256]
 
-data_label = np.zeros((1600, 10)) 
-data_label_prev= data.iloc[0:1600, 256]
+    #df = pd.DataFrame(data_label)
 
-print("\n")
-#df = pd.DataFrame(data_label)
+    # Llenado de vectores binarios
+    for j in range(0, 1600):
+        for pos in range(1, 11):
+            if data_label_prev.loc[j] == pos:
+                data_label[j][pos-1] = 1
 
-#Llenado de vectores binarios
-for j in range (0,1600):
-    for pos in range (1,11):
-        if data_label_prev.loc[j] == pos :
-            data_label[j][pos-1]=1
+    print(data_label)
 
-print(data_label)
+    # Lectura de param_sae.csv
+    param_sae = np.genfromtxt('param_sae.csv', delimiter=',')
 
+    # PARÁMETROS SAE.CS
+    data_param_sae = {
+        percent_training: param_sae[0],
+        param_pinv: param_sae[1],
+        max_epoch: param_sae[2],
+        size_mini_batch: param_sae[3],
+        tasa_aprendizaje_mu: param_sae[4],
+        hidden_node_ae1: param_sae[5],
+        hidden_node_ae2: param_sae[6]
+    }
 
-#Lectura de param_sae.csv
-param_sae=np.genfromtxt('param_sae.csv',delimiter=',')
+    # print(data_param_sae)
 
-#PARÁMETROS SAE.CS
-percent_trainning=param_sae[0]
-param_pinv=param_sae[1]
-max_epoch=param_sae[2]
-size_mini_batch=param_sae[3]
-tasa_aprendizaje_mu=param_sae[4]
-hidden_node_ae1=param_sae[5]
-hidden_node_ae2=param_sae[6]
+    # Lectura de param_softmax.csv
+    param_softmax = np.genfromtxt('param_softmax.csv', delimiter=',')
+    max_iteracion = param_softmax[0]
+    tasa_aprendizaje = param_softmax[1]
+    penalidad_pesos = param_softmax[2]
 
-print(percent_trainning,param_pinv,max_epoch,size_mini_batch,tasa_aprendizaje_mu,hidden_node_ae1,hidden_node_ae2)
+    # print(max_iteracion, tasa_aprendizaje, penalidad_pesos)
 
-#Lectura de param_sae.csv
-param_softmax=np.genfromtxt('param_softmax.csv',delimiter=',')
-max_iteracion=param_softmax[0]
-tasa_aprendizaje=param_softmax[1]
-penalidad_pesos=param_softmax[2]
-
-print(max_iteracion,tasa_aprendizaje,penalidad_pesos)
- 
+pre_proc()
--- train_main.py
+++ train_main.py
@@ -1,25 +1,29 @@
 import pandas as pd
 import numpy as np
-
+# from pre_proc import *
 
 def get_pre_data(type):
-  data = pd.read_csv('data.csv', sep=',', header=None)
+  data_df = pd.read_csv('data.csv', sep=',', header=None)
   param_sae = pd.read_csv('param_sae.csv', sep=',', header=None)
   
   porcentaje_train = param_sae.iloc[0][0]
-  rows_number = len(data.index)
+  rows_number = len(data_df.index)
   train_number = int(np.round(porcentaje_train*rows_number))
   testing_number = rows_number - train_number
 
   # TODO reordenar aleatoriamente la data
+  data_df_copy = data_df.sample(frac=1)
+
   if type == 'train':
-    training_data = data.iloc[0:train_number]
+    training_data = data_df_copy.iloc[0:train_number]
     return training_data
   elif type == 'test':
-    testing_data = data.iloc[train_number:]
+    testing_data = data_df_copy.iloc[train_number:]
     return testing_data
   
 print(get_pre_data('test'))
+print(get_pre_data('train'))
+# print(percent_trainning)
 
 
 
