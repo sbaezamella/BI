e389abfcf0cd28809653a93eb3d416d7d9cd5837
Sebastián Baeza <sbaezamella@gmail.com>
2020-06-21 18:21:54 -0400

    Archivo .py editados
    Funciones para cada archivo, se retorna la data y los parametros en pre_proc

--- pre_proc.py
+++ pre_proc.py
@@ -17,30 +17,39 @@ def pre_proc():
             if data_label_prev.loc[j] == pos:
                 data_label[j][pos-1] = 1
 
-    print(data_label)
-
     # Lectura de param_sae.csv
     param_sae = np.genfromtxt('param_sae.csv', delimiter=',')
 
     # PARÁMETROS SAE.CS
     data_param_sae = {
-        percent_training: param_sae[0],
-        param_pinv: param_sae[1],
-        max_epoch: param_sae[2],
-        size_mini_batch: param_sae[3],
-        tasa_aprendizaje_mu: param_sae[4],
-        hidden_node_ae1: param_sae[5],
-        hidden_node_ae2: param_sae[6]
+        'percent_training': param_sae[0],
+        'param_pinv': param_sae[1],
+        'max_epoch': param_sae[2],
+        'size_mini_batch': param_sae[3],
+        'tasa_aprendizaje_mu': param_sae[4],
+        'hidden_node_ae1': param_sae[5],
+        'hidden_node_ae2': param_sae[6]
     }
-
-    # print(data_param_sae)
 
     # Lectura de param_softmax.csv
     param_softmax = np.genfromtxt('param_softmax.csv', delimiter=',')
-    max_iteracion = param_softmax[0]
-    tasa_aprendizaje = param_softmax[1]
-    penalidad_pesos = param_softmax[2]
+    data_param_softmax = {
+        'max_iteracion': param_softmax[0],
+        'tasa_aprendizaje': param_softmax[1],
+        'penalidad_pesos': param_softmax[2]
+    }
 
-    # print(max_iteracion, tasa_aprendizaje, penalidad_pesos)
+    # Lectura de param_softmax.csv
+    param_bp = np.genfromtxt('param_bp.csv', delimiter=',')
+    data_param_bp = {
+        'max_iteracion': param_bp[0],
+        'tasa_aprendizaje': param_bp[1]
+    }
 
-pre_proc()
+    parametros = {
+        'sae': data_param_sae,
+        'softmax': data_param_softmax,
+        'bp': data_param_bp
+    }
+
+    return data_input, parametros
